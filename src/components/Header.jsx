a8cb3413dbc685ee0d018a337c4ae3fb53322e52
s0fiekm <151931942+s0fiekm@users.noreply.github.com>
2024-11-12 14:55:11 +0100

    små ændringer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,16 +7,16 @@ function Header() {
         <ul className="flex gap-9 font-poppins text-bg text-xl font-semibold">
           <li className="relative group">
             <Link href="/">Home</Link>
-            <span className="absolute left-0 right-0 -bottom-2 mx-auto h-1 w-0 bg-accent transition-all duration-300 group-hover:w-full"></span>
+            <span className="absolute left-0 right-0 -bottom-2 mx-auto h-1 w-0 bg-accent  group-hover:w-full"></span>
           </li>
           <li className="relative group">
             <Link href="/products">Products</Link>
-            <span className="absolute left-0 right-0 -bottom-2 mx-auto h-1 w-0 bg-accent transition-all duration-300 group-hover:w-full"></span>
+            <span className="absolute left-0 right-0 -bottom-2 mx-auto h-1 w-0 bg-accent group-hover:w-full"></span>
           </li>
         </ul>
         <div className="relative group cursor-pointer font-poppins text-bg text-2xl font-semibold	">
           <BsBag />
-          <span className="absolute left-0 right-0 -bottom-3 mx-auto h-1 w-0 bg-accent  transition-all duration-300 group-hover:w-full"></span>
+          <span className="absolute left-0 right-0 -bottom-3 mx-auto h-1 w-0 bg-accent group-hover:w-full"></span>
         </div>
       </nav>
     </header>
