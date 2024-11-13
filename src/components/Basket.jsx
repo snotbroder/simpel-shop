746dc46b246213f33974ac1ae665bc4cfd90d527
julievictoria98 <jura0002@stud.kea.dk>
2024-11-13 09:40:09 +0100

    rettet

--- src/components/Basket.jsx
+++ src/components/Basket.jsx
@@ -6,9 +6,9 @@ function Basket({ basketArray, setBasketArray }) {
       <h1>Basket</h1>
       {basketArray.map((product, i) => (
         <div key={`basket${i}`}>
-          <div className="flex gap-8">
+          <div className="flex gap-8 justify-between">
             <div>
-              <h1 className="max-w-8">{product.title}</h1>
+              <h1>{product.title}</h1>
               <p>{product.price}</p>
             </div>
             <Image src={product.thumbnail} width={75} height={75} alt="test" />
