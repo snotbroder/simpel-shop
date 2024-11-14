b3dd75863dac5f23d49e81c713940e370b171170
s0fiekm <151931942+s0fiekm@users.noreply.github.com>
2024-11-14 10:02:17 +0100

    rettet

--- src/components/PopCategories.jsx
+++ src/components/PopCategories.jsx
@@ -6,7 +6,12 @@ function PopCategories() {
       <h2 className="font-poppins  text-2xl font-semibold text-primary">
         Popular categories
       </h2>
-      <div className="flex flex-wrap gap-4 mt-4 justify-center"></div>
+      <div className="flex flex-wrap gap-4 mt-4 justify-center">
+        <CategoryCard backgroundImage="/perfume.jpg" tagLabel="Fragnances" />
+        <CategoryCard backgroundImage="/beauty.jpg" tagLabel="Beauty" />
+        <CategoryCard backgroundImage="/groceries.jpg" tagLabel="Groceries" />
+        <CategoryCard backgroundImage="/furniture.jpg" tagLabel="Furniture" />
+      </div>
     </div>
   );
 }
