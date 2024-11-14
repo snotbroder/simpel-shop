ab3e8a2fa89fa1e88d52b10578f42ca09ea44129
s0fiekm <151931942+s0fiekm@users.noreply.github.com>
2024-11-14 09:53:37 +0100

    main button

--- src/components/MainButton.jsx
+++ src/components/MainButton.jsx
@@ -3,7 +3,7 @@ import { IoIosArrowRoundForward } from "react-icons/io";
 function MainButton({ buttonText }) {
   return (
     <>
-      <button className="bg-white text-black border border-black rounded-lg pl-4 pr-4 pb-1 pt-1 hover:font-medium group hover:bg-actionColor">
+      <button className="bg-accent text-primary w-auto rounded-lg pl-6 pr-4 pb-1 pt-1 text-center hover:font-medium group hover:bg-actionColor">
         {buttonText}
         <IoIosArrowRoundForward
           className="inline transition-transform duration-200 ease-in-out group-hover:translate-x-1"
