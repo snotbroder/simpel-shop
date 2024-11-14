cdfbe0a9c87f3fa38ff3640d806b57c343b46cff
s0fiekm <151931942+s0fiekm@users.noreply.github.com>
2024-11-14 09:46:54 +0100

    test

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,26 @@
 import Card from "@/components/Card";
+import Hero from "@/components/Hero";
 import List from "@/components/List";
-import MainButton from "@/components/MainButton";
+import PopCategories from "@/components/PopCategories";
+
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="font-poppins font-extrabold">hej</h1>
-      <h2 className="font-noto">test not</h2>
+    <div className="h-full bg-bg">
+      <Hero />
+      <PopCategories />
+      <div className="text-primary">
+        <h2>Everything you need in once</h2>
+        <p>
+          At Shoply Goods, you’ll find everything your heart desires. From
+          glasses and cat food to makeup, perfumes, phones, and fresh produce –
+          we have it all! Our mission is to make your shopping experience easy
+          and convenient, no matter what you’re looking for. Explore our wide
+          selection and discover just how simple it is to find exactly what you
+          need.
+        </p>
+      </div>
     </div>
   );
 }
