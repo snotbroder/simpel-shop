95e855fd83ae6dc61330a249d444bc775b3cc926
s0fiekm <151931942+s0fiekm@users.noreply.github.com>
2024-11-14 09:54:14 +0100

    hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import MainButton from "@/components/MainButton";
+
+function Hero() {
+  return (
+    <section className="relative bg-cover bg-center flex h-screen">
+      <div className="absolute inset-0 bg-[url('/heroimg.png')] bg-cover bg-center opacity-50"></div>
+      <div className="ml-20  gap-5 flex flex-col justify-center place-items-start relative z-10">
+        <h1 className="font-poppins text-primary text-7xl uppercase font-bold">
+          Shoply Goods
+        </h1>
+        <MainButton buttonText="Products"></MainButton>
+      </div>
+    </section>
+  );
+}
+export default Hero;
