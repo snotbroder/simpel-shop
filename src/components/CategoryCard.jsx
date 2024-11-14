ef7f368a25d7607e4c8be4924c8002db13493d80
s0fiekm <151931942+s0fiekm@users.noreply.github.com>
2024-11-14 09:59:43 +0100

    delete url

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -5,10 +5,7 @@ import Tag from "@/components/Tag";
 function CategoryCard({ backgroundImage, tagLabel }) {
   return (
     <div className="relative  w-64 h-40 rounded-lg overflow-hidden shadow-lg">
-      <div
-        className="absolute inset-0 bg-cover bg-center"
-        style={{ backgroundImage: `url(${backgroundImage})` }}
-      ></div>
+      <div className="absolute inset-0 bg-cover bg-center"></div>
       {tagLabel && (
         <div className="absolute top-2 z-20">
           <Tag tagLabel={tagLabel} />
