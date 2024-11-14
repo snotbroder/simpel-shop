e3af04a611ac5d9c05e73933b3095188860e7820
s0fiekm <151931942+s0fiekm@users.noreply.github.com>
2024-11-14 23:25:09 +0100

    responsiv button

--- src/components/MainButton.jsx
+++ src/components/MainButton.jsx
@@ -1,12 +1,19 @@
 import Link from "next/link";
-import { IoIosArrowRoundForward } from "react-icons/io";
 
-function MainButton({ buttonText }) {
+function MainButton({ buttonText, icon: Icon }) {
   return (
     <>
-      <Link href={`/products`} className="bg-accent text-primary transition-all  text-xl  w-auto rounded-lg px-10 py-3 text-center hover:font-medium group hover:bg-primary hover:text-bg hover:transition-all">
+      <Link
+        href={`/products`}
+        className="bg-accent text-primary transition-all  md:text-xl text-lg  w-auto rounded-lg px-5 py-2 lg:px-10 lg:py-3 text-center hover:font-medium group hover:bg-primary hover:text-bg hover:transition-all"
+      >
         {buttonText}
-        <IoIosArrowRoundForward className="inline transition-transform duration-200 ease-in-out group-hover:translate-x-1" size={30} />
+        {Icon && (
+          <Icon
+            className="inline transition-transform duration-200 ease-in-out group-hover:translate-x-1"
+            size={30}
+          />
+        )}
       </Link>
     </>
   );
