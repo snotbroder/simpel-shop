263f176057841274f716a000d38f2e37a106b18b
s0fiekm <151931942+s0fiekm@users.noreply.github.com>
2024-11-14 09:52:10 +0100

    category card

---
+++ src/components/CategoryCard.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Image from "next/image";
+import Tag from "@/components/Tag";
+
+function CategoryCard({ backgroundImage, tagLabel }) {
+  return (
+    <div className="relative  w-64 h-40 rounded-lg overflow-hidden shadow-lg">
+      <div
+        className="absolute inset-0 bg-cover bg-center"
+        style={{ backgroundImage: `url(${backgroundImage})` }}
+      ></div>
+      {tagLabel && (
+        <div className="absolute top-2 z-20">
+          <Tag tagLabel={tagLabel} />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default CategoryCard;
