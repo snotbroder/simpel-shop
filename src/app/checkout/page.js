380e08d0b8ad729ee6c50bf93cddea5f336133a6
Joris <jote0002@stud.kea.dk>
2024-11-14 20:26:56 +0100

    made checkout page more fluid between screen sized

--- src/app/checkout/page.js
+++ src/app/checkout/page.js
@@ -10,26 +10,26 @@ function checkout() {
             <h3 className="text-xl font-poppins text-primary">Your basket</h3>
           </div>
         </div>
-        <div className="col-start-2 col-end-4 bg-gray rounded-md h-full p-10 px-20">
+        <section className="col-start-2 col-end-4 bg-gray rounded-md h-full py-10 px-5 md:px-20 lg:px-30 xl:px-35">
           <h3 className="font-poppins text-xl mb-3 text-primary">Your contact information</h3>
-          <form className="mb-5">
+          <form className="mb-10">
             <div className="flex flex-wrap justify-between mb-5">
               <label className="flex flex-col font-poppins text-primary" id="f-name">
                 First name
-                <input className="w-64 rounded-sm px-2 bg-bg" name="f-name" type="text"></input>
+                <input className="w-64 rounded-sm px-2 bg-bg font-noto" name="f-name" type="text"></input>
               </label>
-              <label className="flex flex-col font-poppins" id="l-name">
-                <strong>Last name</strong>
-                <input className="w-64 rounded-sm px-2" name="l-name" type="text"></input>
+              <label className="flex flex-col font-poppins text-primary" id="l-name">
+                Last name
+                <input className="w-64 rounded-sm px-2 bg-bg font-noto" name="l-name" type="text"></input>
               </label>
             </div>
-            <label className="flex flex-col font-poppins" id="l-name">
-              <strong>Email</strong>
-              <input className="w-64 rounded-sm px-2" name="email" type="text"></input>
+            <label className="flex flex-col font-poppins text-primary" id="email">
+              Email
+              <input className="w-64 rounded-sm px-2 bg-bg font-noto" name="email" type="text"></input>
             </label>
           </form>
           <PaymentMethod></PaymentMethod>
-        </div>
+        </section>
       </section>
     </section>
   );
--- src/components/PaymentMethod.jsx
+++ src/components/PaymentMethod.jsx
@@ -1,40 +1,55 @@
 "use client";
 import { useState } from "react";
 import MainButton from "./MainButton";
+import Link from "next/link";
 
 export default function PaymentMethod({}) {
   const [activeIndex, setActiveIndex] = useState(0);
   let total = 20;
   return (
-    <section className="">
-      <h3 className="font-poppins text-sm mb-3 text-primary">Please choose payment method</h3>
+    <section>
+      <h3 className="font-poppins text-xs mb-3 text-primary">Please choose method of payment</h3>
       <div className="flex gap-4 justify-between lg:flex-nowrap mb-5">
-        <Panel title="Card" isActive={activeIndex === 0} onShow={() => setActiveIndex(0)}>
+        <Panel title="Credit card" isActive={activeIndex === 0} onShow={() => setActiveIndex(0)}>
           <form className="py-7">
             <div className="flex flex-wrap justify-between gap-3 mb-5">
-              <label className="flex flex-col font-poppins" id="card-number">
-                <strong>Card number</strong>
-                <input className="w-full rounded-sm px-2" name="card-number" type="number"></input>
+              <label className="flex flex-col font-poppins text-primary" id="cardnumber">
+                Card number
+                <input className="w-full rounded-sm px-2 bg-bg font-noto" name="cardnumber" type="text"></input>
               </label>
-              <label className="flex flex-col font-poppins" id="cvv">
-                <strong>CVV</strong>
-                <input className="w-32 rounded-sm px-2" name="cvv" type="number"></input>
+
+              <label className="flex flex-col font-poppins text-primary" id="cvv">
+                CVV
+                <input className="w-32 rounded-sm px-2 bg-bg font-noto" name="cvv" type="text"></input>
               </label>
             </div>
             <div className="flex gap-3 mb-5">
-              <label className="flex flex-col font-poppins" id="month">
-                <strong>MM</strong>
-                <select className="rounded-sm px-2" name="email" type="month">
+              <label className="flex flex-col font-poppins text-primary" id="month">
+                MM
+                <select className="rounded-sm px-2 bg-bg font-noto" name="month" type="month">
                   <option>01</option>
                   <option>02</option>
+                  <option>03</option>
+                  <option>04</option>
+                  <option>05</option>
+                  <option>06</option>
+                  <option>07</option>
+                  <option>08</option>
+                  <option>09</option>
+                  <option>10</option>
+                  <option>11</option>
+                  <option>12</option>
                 </select>
               </label>
               <p>/</p>
-              <label className="flex flex-col font-poppins" id="year">
-                <strong>YY</strong>
-                <select className="rounded-sm px-2" name="email" type="year">
+              <label className="flex flex-col font-poppins text-primary" id="year">
+                YY
+                <select className="rounded-sm px-2 bg-bg font-noto" name="year" type="year">
                   <option>24</option>
                   <option>25</option>
+                  <option>26</option>
+                  <option>27</option>
+                  <option>28</option>
                 </select>
               </label>
             </div>
@@ -47,20 +62,25 @@ export default function PaymentMethod({}) {
         </Panel>
         <Panel title="Mobilepay" isActive={activeIndex === 1} onShow={() => setActiveIndex(1)}>
           <form className="py-7">
-            <label className="flex flex-col font-poppins" id="phone-number">
-              <strong>Phone number</strong>
-              <input className="w-full rounded-sm px-2" name="phone-number" type="number"></input>
+            <label className="flex flex-col font-poppins text-primary" id="phonenumber">
+              Phone number
+              <input className="w-full rounded-sm px-2 bg-bg font-noto" name="phonenumber" type="text"></input>
             </label>
             <label className="flex font-poppins" id="remember">
               <input className="mr-2" name="remember" type="checkbox"></input>
-              <p>Remember my information</p>
+              <p>Remember my phone number</p>
             </label>
           </form>
         </Panel>
       </div>
       <label className="flex font-poppins" id="remember">
         <input className="mr-2" name="remember" type="checkbox"></input>
-        <p>I accept the terms and conditions</p>
+        <p>
+          I accept the{" "}
+          <Link className="underline" target="_blank" href="https://www.termsfeed.com/blog/sample-terms-and-conditions-template/">
+            terms and conditions
+          </Link>
+        </p>
       </label>
 
       <span className="grid place-self-end">
